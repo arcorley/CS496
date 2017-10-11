ad93adf12c5c0aa8b17762272fec33c5d0bb70c3
arcorley <arcorley528@gmail.com>
2017-10-11 15:14:12 -0700

    Requirement set 1 complete for boats and slips

--- REST_API_Usage/assignment2.py
+++ REST_API_Usage/assignment2.py
@@ -4,6 +4,7 @@
 from google.appengine.ext import ndb
 import webapp2
 import json
+import logging
 # [END imports]
 
 # [START Boat declaration]
@@ -161,9 +162,26 @@ class BoatHandler(webapp2.RequestHandler):
 
     # [START delete handler]
     def delete(self, id):
-        ndb.Key(urlsafe=id).delete()
+        b = ndb.Key(urlsafe=id).get()
+        #if the boat is in a slip, empty the slip
+        if (b.at_sea == False):
+            #get the slip that has this boat as the current_boat
+            qry = Slip.query()
+            qryResults = qry.fetch()
+            for x in qryResults: #for each result, check if the current_boat is equal to this boat's id
+                if (x.current_boat == b.id):
+                    slipId = x.id
+                    break
+
+            s = ndb.Key("Slip", long(slipId)).get() #get the slip object this boat is located in
+            s.current_boat = "" #empty the slip
+            s.arrival_date = ""      
+            s.put()
 
         self.response.headers.add('Status', '200 OK')
+        ndb.Key(urlsafe=id).delete()
+    # [END delete handler]
+
 # [END BoatHandler]
 
 # [START SlipHandler]
@@ -232,6 +250,11 @@ class SlipHandler(webapp2.RequestHandler):
             for x in qryResults: #for each result, serialize the output so it can be sent to JSON
                 x_d = x.to_dict()
                 x_d['self'] = "/slips/" + x.key.urlsafe()
+                if (x.current_boat != ""): #if a new boat was passed in
+                    b = ndb.Key("Boat", long(x.current_boat)).get()
+                    x_d['boat_in_slip'] = "/boats/" + b.key.urlsafe()
+                else:
+                    x_d['boat_in_slip'] = ""
                 results.append(x_d) #add the dictionary to results
             self.response.headers.add('Status', '200 OK')
             self.response.headers.add('Content-Type', 'Application/JSON')
@@ -266,6 +289,123 @@ class SlipHandler(webapp2.RequestHandler):
             r['message'] = "No fields submitted to update"
             self.response.write(json.dumps(r))            
     # [END patch handler]
+
+    # [START put handler]
+    def put(self, id): #replace request handler
+        s = ndb.Key(urlsafe=id).get() #get the existing slip by id
+        slip_data = json.loads(self.request.body) #get the request parameters
+
+        continueVar = 1 #set to 0 if any validations fail. This is a cue to prevent changes from being made
+
+        #do some request parameter checking
+        if ('number' in slip_data.keys()): #check that a number was sent along with request
+            numberVar = slip_data['number']
+        else:
+            contineVar = 0
+            numberVar = None
+            self.response.headers.add('Status', '400 Bad Request')
+            self.response.headers.add('Content-Type', 'Application/JSON')
+            r = {}
+            r['message'] = "Request didn't contain a slip number"
+            self.response.write(json.dumps(r))
+            return
+
+        if ('current_boat' in slip_data.keys() and 'arrival_date' in slip_data.keys()): #if a boat is provided, an arrival date must be provided also
+            boatVar = slip_data['current_boat']
+            dateVar = slip_data['arrival_date']
+        elif ('current_boat' not in slip_data.keys() and 'arrival_date' not in slip_data.keys()):
+            boatVar = ""
+            dateVar = ""
+        else:
+            continueVar = 0
+            self.response.headers.add('Status', '400 Bad Request')
+            self.response.headers.add('Content-Type', 'Application/JSON')
+            r = {}
+            r['message'] = "current_boat and arrival_date must both be provided or must both be empty"
+            self.response.write(json.dumps(r))
+            return
+
+        #this section completes the put functionality if the request passes validation
+        if (continueVar == 1):
+            existingNumber = 0 #if 0, the slip number provided doesn't match an existing slip number
+
+            #this section processes the slip number
+            if (numberVar != s.number): #if a different number was provided, need to check that it doesn't match an existing slip
+                qry = Slip.query() #query the slips data to check if the slip number entered already exists
+                qryResults = qry.fetch()
+                for x in qryResults: #iterate through each query result
+                    x_d = x.to_dict()
+                    if (x_d['number'] == numberVar): #compare the number in the results to the number provided request
+                        existingNumber = 1
+                        break
+
+            if (existingNumber == 1): #if the slip number exists, return an error
+                self.response.headers.add('Status', '400 Bad Request')
+                self.response.headers.add('Content-Type', 'Application/JSON')
+                r = {}
+                r['message'] = "The slip number provided already exists"
+                self.response.write(json.dumps(r))
+                return
+            else: #else, overwrite the slip number
+                s.number = numberVar
+                s.put()
+
+            #this section processes the current_boat field
+            if (s.current_boat == ""): #if field is currently empty
+                if (boatVar != ""): #if requested field is not empty. if it is empty, we can just leave it alone
+                    s.current_boat = boatVar #update the slip with the new id
+                    b = ndb.Key("Boat", long(s.current_boat)).get() #get the boat with the id passed in
+                    b.at_sea = False #update the boat's at_sea field
+                    b.put()
+            else: #if there is currently a boat in the slip
+                cb = ndb.Key("Boat", long(s.current_boat)).get() #get the current_boat
+                cb.at_sea = True #kick boat out of slip
+                cb.put() #commit the change
+
+                if (boatVar != ""): #if requested field is not empty
+                    s.current_boat = boatVar #update the slip with the new id
+                    b = ndb.Key("Boat", long(boatVar)).get() #get the boat with the id passed in
+                    b.at_sea = False
+                    b.put() #commit the change
+                else:
+                    s.current_boat = boatVar #set the current_boat field to empty
+
+            s.put() #commit the changes to the slip
+
+            #this section processes the arrival_date field
+            s.arrival_date = dateVar
+            s.put()
+
+            #add the links to the response
+            s_d = s.to_dict()
+            s_d['self'] = "/slips/" + id #add the self link
+            s_d['boat_in_slip'] = ""
+
+            if (boatVar != ""): #if a new boat was passed in
+                b = ndb.Key("Boat", long(boatVar)).get()
+                s_d['boat_in_slip'] = "/boats/" + b.key.urlsafe()
+
+            #send the response
+            self.response.headers.add('Status', '200 OK')
+            self.response.headers.add('Content-Type', 'Application/JSON')
+            self.response.write(json.dumps(s_d))
+            return
+    # [END put handler]
+
+    # [START delete handler]
+    def delete(self, id):
+        s = ndb.Key(urlsafe=id).get() #get the existing slip by id
+        #currBoatInt = int(s.current_boat)
+        
+        #if there is a boat in the slip, need to change its at_sea property
+        if (s.current_boat != ""):
+            b = ndb.Key("Boat", long(s.current_boat)).get()
+            b.at_sea = True
+            b.put()
+
+        self.response.headers.add('Status', '200 OK')
+        ndb.Key(urlsafe=id).delete()
+    # [END delete handler]    
 
 # [END SlipHandler]
 
